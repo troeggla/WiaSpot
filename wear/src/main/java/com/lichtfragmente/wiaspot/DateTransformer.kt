d90d246ba959bbbdb4fd97a9495e89d9d47e6510
Thomas Röggla <t.roggla@cwi.nl>
2020-01-16 14:36:16 +0100

    added class to get a textual representation of the current date

---
+++ wear/src/main/java/com/lichtfragmente/wiaspot/DateTransformer.kt
@@ -0,0 +1,44 @@
+package com.lichtfragmente.wiaspot
+
+import java.util.Calendar
+
+class DateTransformer(time: Calendar) {
+    private val weekday = time.get(Calendar.DAY_OF_WEEK)
+    private val day = time.get(Calendar.DAY_OF_MONTH)
+    private val month = time.get(Calendar.MONTH)
+
+    private fun getWeekday(): String {
+        return when (weekday) {
+            Calendar.MONDAY -> "muntig"
+            Calendar.TUESDAY -> "ërtig"
+            Calendar.WEDNESDAY -> "mittig"
+            Calendar.THURSDAY -> "pfinstig"
+            Calendar.FRIDAY -> "freitig"
+            Calendar.SATURDAY -> "såmstig"
+            Calendar.SUNDAY -> "sunntig"
+            else -> ""
+        }
+    }
+
+    private fun getMonth(): String {
+        return when (month) {
+            Calendar.JANUARY -> "jänner"
+            Calendar.FEBRUARY -> "februar"
+            Calendar.MARCH -> "märz"
+            Calendar.APRIL -> "april"
+            Calendar.MAY -> "mai"
+            Calendar.JUNE -> "juni"
+            Calendar.JULY -> "juli"
+            Calendar.AUGUST -> "august"
+            Calendar.SEPTEMBER -> "september"
+            Calendar.OCTOBER -> "oktober"
+            Calendar.NOVEMBER -> "november"
+            Calendar.DECEMBER -> "dezember"
+            else -> ""
+        }
+    }
+
+    fun getTextualDate(): String {
+        return "${getWeekday()}, ${day}. ${getMonth()}"
+    }
+}
