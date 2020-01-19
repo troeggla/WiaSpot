11c87d0bf50c80860e9b3fd7da92adab20370b44
Thomas Röggla <tom@lichtfragmente.com>
2020-01-19 15:58:37 +0100

    set_time script now also allows setting of date

--- set_time.sh
+++ set_time.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-  echo "USAGE: $0 HH MM"
+if [ $# -eq 2 ]; then
+  adb shell "su 0 date 0101${1}${2}"
+elif [ $# -eq 4 ]; then
+  adb shell "su 0 date ${3}${4}${1}${2}"
+else
+  echo "USAGE: $0 HH MM [DD MM]"
   exit 1
 fi
-
-adb shell "su 0 date 0101${1}${2}"
